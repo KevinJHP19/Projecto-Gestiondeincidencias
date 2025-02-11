47548b368d426df82323f99a0b212ea463479f1b
A-3-huaman25 <huamanpintokevin@fpllefia.com>
2025-02-11 19:35:41 +0100

    Capçalera i Menú (Header)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 //Instalamos el react-router-dom e importamos
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 //Importamos las vistas de la carpeta Pages
 import Panel from './pages/Panel'
 import Login from './pages/Iniciosession'
 import Registro from './pages/Registro'
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   
@@ -15,27 +16,7 @@ function App() {
       
       <Router>
         
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <Link class="navbar-brand" to='/'>Gestión de incidencias FPLLEFIA</Link>
-        
-        <div>
-          <button className="btn btn-secondary ms-2">
-            <Link className='nav-link' to="/">PANEL</Link>
-            </button>
-          <button className="btn btn-secondary ms-2">
-            <Link className='nav-link' to="/iniciarsesion">LOGIN</Link>
-          </button>
-          <button className="btn btn-secondary ms-2">
-            <Link className='nav-link' to="/registro">REGISTRO</Link>
-          </button>
-        </div>
-        <div>
-          <span>administrador@fpllefia.com</span>
-          
-        </div>
-      </div>
-    </nav>
+    <Header />
   
   <Routes>
       <Route path="/" element={<Panel />} />
