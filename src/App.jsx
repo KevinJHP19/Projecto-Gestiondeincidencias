9af64442598766a2674adbff8b0a75bd00e6a151
A-3-huaman25 <huamanpintokevin@fpllefia.com>
2025-02-21 16:54:36 +0100

    Crear col·leccions a localStorage: dades_tiquets i dades_usuaris.

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,58 @@ import Registro from './pages/Registro'
 
 import './App.css'
 import Header from './components/Header'
+//importamos el ls.jsx
+
 
 function App() {
+  const datos_tickets =[
+    {
+        id: 1,
+        codigo: 123459,
+        fecha: "18/04/2023",
+        aula: "T6",
+        grupo: "DAW1",
+        ordenador: "PC3",
+        descripcion: "Error de impresora",
+        alumno: "Ana Martínez",
+        resuelto:true
+    },
+    {
+        id: 2,
+        codigo: 123460,
+        fecha: "19/04/2023",
+        aula: "T8",
+        grupo: "DAW2",
+        ordenador: "PC4",
+        descripcion: "Problema de acceso a archivos",
+        alumno: "Pedro Gómez",
+        resuelto:false
+    }
+    
+
+]
+  const datos_usuario = [
+    {
+        id: 1,
+        usuario: "Juan",
+        email: "Juan@gmail.com",
+        password: "1234"
+    },
+    {
+        id: 2,
+        usuario: "Pedro",
+        email: "Pedro@gmail.com",
+        password: "5678"
+    }
+
+  ]
+    localStorage.setItem('datos_tickets', JSON.stringify([datos_tickets]))
+    localStorage.setItem('datos_usuario', JSON.stringify([datos_usuario]))
+  
+  
+
+  
+  
   
   
     return (
