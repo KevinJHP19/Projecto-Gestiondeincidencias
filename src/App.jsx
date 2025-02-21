d7d8e853e9e2933def13aa542eea1addb02fae9c
A-3-huaman25 <huamanpintokevin@fpllefia.com>
2025-02-21 17:22:58 +0100

    Creamos los componentes(sin codigo y actualizamos la coleccion de datos_tickets)

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,51 @@ import Registro from './pages/Registro'
 
 import './App.css'
 import Header from './components/Header'
-//importamos el ls.jsx
+
 
 
 function App() {
   const datos_tickets =[
     {
-        id: 1,
+      id:1,
+      codigo: 123457,
+      fecha: "15/04/2023",
+      fecha_resuelto:"15/05/2023",
+      aula: "T7",
+      grupo: "DAW2",
+      ordenador: "PC1",
+      descripcion: "Problema de conexión a Internet",
+      alumno: "Juan Pérez",
+      resuelto:true
+
+    },
+    {
+      id:2,
+      codigo:123458,
+      fecha: "17/04/2023",
+      fecha_resuelto:"15/05/2023",
+      aula: "T8",
+      grupo: "DAW1",
+      ordenador: "PC2",
+      descripcion: "Pantalla en blanco",
+      alumno: "Juan Rodríguez",
+      resuelto:true
+
+    },
+    {
+      id:3,
+      codigo: 123459,
+      fecha: "18/04/2023",
+      fecha_resuelto:"15/05/2023",
+      aula: "T8",
+      grupo: "DAW1",
+      ordenador: "PC3",
+      descripcion: "Error de impresora",
+      alumno: "Ana Martínez",
+      resuelto:true
+    },
+    {
+        id: 4,
         codigo: 123459,
         fecha: "18/04/2023",
         aula: "T6",
@@ -22,10 +60,10 @@ function App() {
         ordenador: "PC3",
         descripcion: "Error de impresora",
         alumno: "Ana Martínez",
-        resuelto:true
+        resuelto:false
     },
     {
-        id: 2,
+        id: 5,
         codigo: 123460,
         fecha: "19/04/2023",
         aula: "T8",
@@ -34,6 +72,39 @@ function App() {
         descripcion: "Problema de acceso a archivos",
         alumno: "Pedro Gómez",
         resuelto:false
+    },
+    {
+        id: 6,
+        codigo: 123461,
+        fecha: "20/04/2023",
+        aula: "T6",
+        grupo: "DAW1",
+        ordenador: "PC1",
+        descripcion: "Aplicación se cierra inesperadamente",
+        alumno: "Sofía Fernández",
+        resuelto:false
+    },
+    {
+      id:7,
+      codigo: 123462,
+      fecha: "21/04/2023",
+      aula: "T7",
+      grupo: "DAW2",
+      ordenador: "PC2",
+      descripcion: "Problema de conexión a la red",
+      alumno: "Luis Torres",
+      resuelto: false
+    },
+    {
+      id:8,
+      codigo: 123463,
+      fecha: "22/04/2023",
+      aula: "T8",
+      grupo: "DAW1",
+      ordenador: "PC3",
+      descripcion: "Archivos corruptos",
+      alumno: "Carolina Ramírez",
+      resuelto: false
     }
     
 
---
+++ src/components/TiquetsPendents.jsx
@@ -0,0 +1,23 @@
+export default function TiquetsPendents(){
+
+    console.log('TiquetsPendents cargados');
+    return (
+        <div>
+            <h2>Tickets pendientes</h2>
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Fecha</th>
+                        <th>Título</th>
+                        <th>Prioridad</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Aquí se cargan los tickets */}
+                </tbody>
+            </table>
+        </div>
+    )
+}
